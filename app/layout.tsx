51f80aaa200a3eddc23ccac6b50f93d3550d897f
waxim.hq@gmail.com <myflow.rep@gmail.com>
2026-05-17 20:56:02 +0530

    hero fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,6 @@ const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
   display: "swap",
-  weight: ["300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
